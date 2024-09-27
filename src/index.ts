699ecd306e2d26e48e6c9cc15268d9dd6a4a84e6
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-09-27 11:50:26 -0400

    updated tsconfig, package.json

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { BroadcastStream } from "./BroadcastStream";
-import { Session } from "./Session";
+import { Session, CONNECTION_STATE } from "./Session";
 import { IBroadcastStream } from "./interfaces/IBroadcastStream";
 import { IEvent } from "./interfaces/IEvent";
 import { IOptions } from "./interfaces/IOptions";
@@ -15,5 +15,6 @@ export {
     IBroadcastStream,
     IEvent,
     IOptions,
-    ISession as IStreamr
+    ISession,
+    CONNECTION_STATE
 }
